9c2b1824547e45fe423ce0807f3b755bec169a8e
Lucas Siviglia <siviglialucas@gmail.com>
2024-09-13 07:59:58 +1000

    README file

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,15 +2,19 @@ import { useEffect, useState} from 'react';
 
 export const ProductList = () => {
   const [products, setProducts] = useState([]);
+  const [url, setUrl] = useState('http://localhost:8000/products');
 
   useEffect(() => {
-    fetch('http://localhost:8000/products')
+    fetch(url)
     .then(response => response.json())
     .then(data => setProducts(data));
   }, [])
 
   return (
     <section>
+      <button onClick={() => setUrl('http://localhost:8000/products')}>All</button>
+      <button onClick={() => setUrl('http://localhost:8000/products?in_stock=true')} >In Stock Only</button>
+
       { products.map((product) => (
         <div className='card' key={product.id}>
           <p className='id'>{product.id}</p>
