bea9d7723176ac1d847b8bfc7dd4dd2728939cff
Lucas Siviglia <siviglialucas@gmail.com>
2024-09-13 07:42:40 +1000

    Adding style and view

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,17 +3,24 @@ import { useEffect, useState} from 'react';
 export const ProductList = () => {
   const [products, setProducts] = useState([]);
 
-  console.log(products);
-
   useEffect(() => {
     fetch('http://localhost:8000/products')
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => setProducts(data));
   }, [])
 
   return (
-    <>
-      <div>ProductList</div>
-    </>
+    <section>
+      { products.map((product) => (
+        <div className='card' key={product.id}>
+          <p className='id'>{product.id}</p>
+          <p className='name'>{product.name}</p>
+          <p className='info'>
+            <span>${product.price}</span>
+            <span className={product.in_stock ? 'instock' : 'unavailable'}>{product.in_stock ? 'In Stock' : 'Unavailable'}</span>
+            </p>
+        </div>
+      )) }
+    </section>
   )
 }
